973f9d4c3d3f6c336a03b1cc9a936e7303b4fa25
thiendoan1208 <thienmacro1234@gmail.com>
2025-05-30 14:41:23 -0400

    setup

--- server.mjs
+++ server.mjs
@@ -1,22 +1,12 @@
-import { createServer } from "node:http";
+import express from "express";
 
-const hostname = "127.0.0.1";
-const port = 4000; // 0 => ~ 65000
+const app = express();
+const port = 3000;
 
-// Tạo 1 HTTP server và listen trên port 3000 for request
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-// Website sẽ chạy ở đâu, sau khi server chạy, hàm callback sẽ được chạy
-
-server.listen(port, hostname, () => {
-  console.log(`server running at: https://${hostname}:${port}/`);
-});
-
-// Tự động chạy trên localhost
-server.listen(port, () => {
-  console.log(`server running at: https://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
